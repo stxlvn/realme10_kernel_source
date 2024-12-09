stxlvn/realme10_kernel_source

commit 80d3e744a4f98c284f8e49c92b8299330acc2f4a

Revert "Merge 5.10.220 into android12-5.10-lts"
This reverts commit a6847bd1cf9efaf9637107be1761926eae57a853, reversing
changes made to b71dba8625ddb45210bc7f24b192a0fb8bb34b1c.

5.10.220 is a bunch of vfs and nfs changes that are not needed in
Android systems, so revert the whole lot all at once, except for the
version number bump.

Change-Id: If28dc2231f27d326d3730716f23545dd0a2cdc75
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

include/linux/kallsyms.h
@@ -71,13 +71,14 @@ static inline void *dereference_symbol_descriptor(void *ptr)
 	return ptr;
 }
 
-int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
-				      unsigned long),
-			    void *data);
-
 #ifdef CONFIG_KALLSYMS
 /* Lookup the address for a symbol. Returns 0 if not found. */
 unsigned long kallsyms_lookup_name(const char *name);
+
+/* Call a function on each kallsyms symbol in the core kernel */
+int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
+				      unsigned long),
+			    void *data);
 
 extern int kallsyms_lookup_size_offset(unsigned long addr,
 				  unsigned long *symbolsize,
@@ -103,6 +104,14 @@ extern bool kallsyms_show_value(const struct cred *cred);
 #else /* !CONFIG_KALLSYMS */
 
 static inline unsigned long kallsyms_lookup_name(const char *name)
+{
+	return 0;
+}
+
+static inline int kallsyms_on_each_symbol(int (*fn)(void *, const char *,
+						    struct module *,
+						    unsigned long),
+					  void *data)
 {
 	return 0;
 }

commit 06c926c03837e4a970244f0c2ba7ccbbb78a78c5

ANDROID: fix kernelci build break: redefinition of 'kallsyms_on_each_symbol'
An extra definition of the function 'kallsyms_on_each_symbol' crept in
for the !KALLSYMS case as a result of the revert of the 5.10.220 merge
combined with the subsequent merge of 5.10.226.

kbuild.compiler.error ./include/linux/kallsyms.h:167:19: error: redefinition
of 'kallsyms_on_each_symbol' in arch/x86/kernel/asm-offsets.s (arch/x86/kernel/asm-offsets.c)

Removed obsolete declaration.

Fixes: 80d3e744a4f9 ("Revert "Merge 5.10.220 into android12-5.10-lts"")
Signed-off-by: Todd Kjos <tkjos@google.com>
Change-Id: Ieb332a4693aa3f1123e74a719d46fc9e1d083eda

include/linux/kallsyms.h
@@ -108,14 +108,6 @@ static inline unsigned long kallsyms_lookup_name(const char *name)
 	return 0;
 }
 
-static inline int kallsyms_on_each_symbol(int (*fn)(void *, const char *,
-						    struct module *,
-						    unsigned long),
-					  void *data)
-{
-	return 0;
-}
-
 static inline int kallsyms_lookup_size_offset(unsigned long addr,
 					      unsigned long *symbolsize,
 					      unsigned long *offset)