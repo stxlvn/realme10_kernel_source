a1f0fecde0f884f055b3afea763aaefbef126cc1
Greg Kroah-Hartman <gregkh@google.com>
2026-05-16 13:59:30 +0000

    ANDROID: GKI: preserve the kernel API for struct task_struct
    In commit faf5303ca79b ("ptrace: slightly saner 'get_dumpable()'
    logic"), a new variable is added to struct task_struct, which makes the
    ABI checker a bit grumpy.  Fix this up by putting the new bit down in
    the reserved space for this kind of expansion as that is exactly what
    the extra padding in this structure was reserved for.
    
    Fixes: faf5303ca79b ("ptrace: slightly saner 'get_dumpable()' logic")
    Change-Id: I80e6c1a33139989c40d1fb7fb5374b6914478ed4
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -821,9 +821,6 @@ struct task_struct {
 	 */
 	unsigned			sched_remote_wakeup:1;
 
-	/* Save user-dumpable when mm goes away */
-	unsigned			user_dumpable:1;
-
 	/* Bit to tell LSMs we're in execve(): */
 	unsigned			in_execve:1;
 	unsigned			in_iowait:1;
@@ -1381,7 +1378,11 @@ struct task_struct {
 	/* PF_IO_WORKER */
 	ANDROID_KABI_USE(1, void *pf_io_worker);
 
-	ANDROID_KABI_RESERVE(2);
+	ANDROID_KABI_USE(2, struct {
+		/* Save user-dumpable when mm goes away */
+		unsigned	user_dumpable:1;
+		});
+
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
